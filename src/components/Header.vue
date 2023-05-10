14efb46a56dae662ab6d027b8c43d9c2f980e01f
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-05-10 12:31:13 +0000

    upd

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <RouterLink :to="{name:'home'}">
             <img src="Images/header/logo.jpg" alt="" class="rounded-full w-16">
         </RouterLink>
-        <div class="flex items-center gap-6 text-slate-400">
+        <div class="hidden md:flex items-center gap-6 text-slate-400">
             <RouterLink :to="{name:'home'}" class="[&.router-link-active]:text-white [&.router-link-active]:font-semibold [&.router-link-active]:after:w-full delay-100 transition-all duration-500 after:delay-100 after:duration-500 after:transition-all flex flex-col after:bg-white after:w-0 after:h-px after:rounded-full hover:after:w-full hover:text-white">Главная</RouterLink>
             <RouterLink :to="{name:'home'}" class="[&.router-link-active]:text-white [&.router-link-active]:font-semibold [&.router-link-active]:after:w-full delay-100 transition-all duration-500 after:delay-100 after:duration-500 after:transition-all flex flex-col after:bg-white after:w-0 after:h-px after:rounded-full hover:after:w-full hover:text-white">Примеры работ</RouterLink>
             <RouterLink :to="{name:'home'}" class="[&.router-link-active]:text-white [&.router-link-active]:font-semibold [&.router-link-active]:after:w-full delay-100 transition-all duration-500 after:delay-100 after:duration-500 after:transition-all flex flex-col after:bg-white after:w-0 after:h-px after:rounded-full hover:after:w-full hover:text-white">Обо мне</RouterLink>
